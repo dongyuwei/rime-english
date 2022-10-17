0aa180369b4a7d035bab6b6eff68386669fc15c7
Dong Yuwei <newdongyuwei@gmail.com>
2022-10-17 17:42:19 +0800

    add inspect.lua and log.lua 第三方lua packages可以放 ~/Library/Rime/lua/ 目录内

--- rime.lua
+++ rime.lua
@@ -1,7 +1,6 @@
 -- https://github.com/shewer/rime-english/issues/2
 -- load_module=require('tools/loadmodule')
 -- load_module.load('english','english',"english.txt")
-
 local english = require("english")()
 english_processor = english.processor
 english_segmentor = english.segmentor
@@ -12,7 +11,13 @@ english_filter0 = english.filter0
 -- yuwei test a custom translator
 function date_translator(input, seg)
     if (input == "date") then
-       --- Candidate(type, start, end, text, comment)
-       yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), " 日期"))
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"),
+                        " 日期"))
     end
- end
+end
+
+local inspect = require 'inspect'
+local log = require "log"
+log.outfile = '/tmp/rime-lua.log'
+log.info(inspect(_G))
